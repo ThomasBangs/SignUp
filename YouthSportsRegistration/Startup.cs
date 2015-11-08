c001c2371dab09ce72f61cc3b11f185280523d42
Thomas Bangs <BangsSolutions@gmail.com>
2015-11-08 08:02:06 -0600

    initial commit

---
+++ YouthSportsRegistration/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(YouthSportsRegistration.Startup))]
+namespace YouthSportsRegistration
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
