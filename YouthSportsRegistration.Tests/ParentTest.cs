32b90bb4c4468d213e68a4f8be4f2a8e52cd45c8
Thomas Bangs <BangsSolutions@gmail.com>
2015-11-08 10:42:59 -0600

    added child class

---
+++ YouthSportsRegistration.Tests/ParentTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using YouthSportsRegistration.Models;
+namespace YouthSportsRegistration.Tests
+{
+    [TestClass]
+    public class ParentTest
+    {
+        [TestMethod]
+        public void ParentEnsureICanCreatInstance()
+        {
+            Parent parent = new Parent();
+            Assert.IsNotNull(parent);
+        }
+    }
+}
--- YouthSportsRegistration/Models/Child.cs
+++ YouthSportsRegistration/Models/Child.cs
@@ -1,19 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
+
+
 namespace YouthSportsRegistration.Models
 {
-    public class Parent
+    public class Child
     {
         
         public int Id { get; set; }
+        public int ParentId { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Address { get; set; }
         public string Phone { get; set; }
         public ApplicationUser Email { get; set; }
-
     }
 }
