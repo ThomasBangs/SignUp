3358c41328c70e0d193a5cb58f84e408c2f15d20
Thomas Bangs <BangsSolutions@gmail.com>
2015-12-10 01:06:32 -0600

    added properties to db context so that I can use linq operators to interact witb my tables

--- YouthSportsRegistration/DataContexts/ChildDb.cs
+++ YouthSportsRegistration/DataContexts/ChildDb.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using System.Web;
+using YouthSportsRegistration.Models;
 
 namespace YouthSportsRegistration.DataContexts
 {
     public class ChildDb : DbContext
     {
+        public DbSet<Child> ChildrenData { get; set; }
+
     }
 }
--- YouthSportsRegistration/DataContexts/ParentDb.cs
+++ YouthSportsRegistration/DataContexts/ParentDb.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using System.Web;
+using YouthSportsRegistration.Models;
 
 
 namespace YouthSportsRegistration.DataContexts
 {
     public class ParentDb :  DbContext
     {
+        public DbSet< Parent> Parents { get; set; }
     }
 }
